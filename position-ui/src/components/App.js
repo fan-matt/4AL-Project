a521bc71a07f50e1c2fe761583a12789cf660c35
Matthew Fan <fanma@ucla.edu>
2019-11-05 15:48:15 -0600

    Added Grid to App component

--- position-ui/src/components/App.js
+++ position-ui/src/components/App.js
@@ -1,12 +1,42 @@
 import React from 'react';
 
+import Grid from './Grid';
 
-function App() {
-  return (
-    <div className="App">
-        
-    </div>
-  );
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            width: window.innerWidth ,
+            height: window.innerHeight ,
+        };
+    }
+
+    // React Lifecycle Methods //
+    componentDidMount() {
+        window.addEventListener('resize' , this.resize)
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize');
+    }
+    // End React Lifecycle Methods //
+
+    resize = () => {
+        this.setState({
+            width: window.innerWidth ,
+            height: window.innerHeight ,
+        });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Grid width={this.state.width} height={this.state.height} />
+            </div>
+        );
+    }
 }
 
 export default App;
