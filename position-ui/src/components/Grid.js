65eb01dd8964288f7fa3bbc8986ad01da17617f5
Matthew Fan <fanma@ucla.edu>
2019-11-05 16:21:58 -0600

    Added coordinate lines

--- position-ui/src/components/Grid.js
+++ position-ui/src/components/Grid.js
@@ -12,18 +12,44 @@ export default class Grid extends React.Component {
         }
     }
 
+    createIntArray = (start , stop) => {
+        let array = new Array(stop - start + 1);
+
+        for(let i = 0; i < stop - start + 1; i ++) {
+            array[i] = start + i;
+        }
+
+        return array;
+    }
+
     renderGrid = () => {
+        const BUFFER = 10;
+        const NUM_GRIDLINES = 10;
+        const HEIGHT = this.props.height - BUFFER * 2;
+        const WIDTH = this.props.width - BUFFER * 2;
+
+        let xLines = this.createIntArray(-NUM_GRIDLINES , NUM_GRIDLINES).map(num => {
+            let yCoordinate = HEIGHT / (2 * NUM_GRIDLINES) * num + this.props.height / 2;
+            return <Line points={[0 , yCoordinate , this.props.width , yCoordinate]} stroke='grey' />
+        });
+
+        let yLines = this.createIntArray(-NUM_GRIDLINES , NUM_GRIDLINES).map(num => {
+            let xCoordinate = WIDTH / (2 * NUM_GRIDLINES) * num + this.props.width / 2;
+            return <Line points={[xCoordinate , 0 , xCoordinate , this.props.height]} stroke='grey' />
+        });
+
         return(
             <>
+                {/* Generate lines */}
+                {xLines}
+                {yLines}
+
                 {/* Coordinate axes */}
                 <Line points={[this.props.width / 2 , 0 , this.props.width / 2 , this.props.height]}
-                    stroke='black' />
+                    stroke='black' strokeWidth={5} />
 
                 <Line points={[0 , this.props.height / 2 , this.props.width , this.props.height / 2]}
-                    stroke='black' />
-
-
-                {/* Generate rest of the lines */}
+                    stroke='black' strokeWidth={5} />
             </>
         );   
     }
